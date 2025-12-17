178fb478e819a27b7190c07e507d292076008902
Moji No <Mojino20@outlook.com>
2025-12-17 17:58:17 -0500

    ARTICULOS

---
+++ MMC_Software/Repositorys/RepositoryCreacionArticulos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMC_Software.Repositorys
+{
+    public class RepositoryCreacionArticulos
+    {
+
+        SqlTransaction _Trans;
+        SqlConnection _conn;
+
+        public RepositoryCreacionArticulos(SqlTransaction trans)
+        {
+            _Trans = trans; 
+            _conn =trans.Connection;
+        }
+    }
+
+}
---
+++ MMC_Software/ViewModel/ArticulosViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMC_Software.ViewModel
+{
+    public class ArticulosViewModel : INotifyPropertyChanged
+    {
+        public int ArticulosID { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ArticulosViewModel(int articulosID)
+        {
+            ArticulosID = articulosID;
+        }
+
+        public ArticulosViewModel()
+        {
+
+        }
+
+    }
+
+}
