d98890a0547680a495da1098a0f92a372f7e221e
Moji No <Mojino20@outlook.com>
2025-12-19 17:38:09 -0500

    Articulo avancez

--- MMC_Software/Repositorys/RepositoryCreacionArticulos.cs
+++ MMC_Software/Repositorys/RepositoryCreacionArticulos.cs
@@ -1,22 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MMC_Software.Repositorys
 {
     public class RepositoryCreacionArticulos
     {
-
+        string _SqlConection;
         SqlTransaction _Trans;
-        SqlConnection _conn;
-
-        public RepositoryCreacionArticulos(SqlTransaction trans)
+        SqlConnection _Conn;
+        public RepositoryCreacionArticulos(string SqlConnection)
         {
-            _Trans = trans; 
-            _conn =trans.Connection;
+            _SqlConection = SqlConnection;
+        }
+
+        public RepositoryCreacionArticulos(SqlTransaction Trans)
+        {
+            _Trans = Trans;
+            _Conn= Trans.Connection;
+        }
+        public DataTable GetCategorias()
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(_SqlConection))
+            {
+                conn.Open();
+
+                string query = @"
+                            SELECT 
+                            CONCAT(CodigoCategoria, ' ', NombreCategoria) AS InfoCategorias,
+	                        TarifaImpuesto,
+                            CategoriasID
+                            FROM ConfCategoriasInve";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                using (SqlDataAdapter adp = new SqlDataAdapter(cmd))
+                {
+                    adp.Fill(dt);
+                }
+            }
+
+            return dt;
+        }
+
+        public DataTable GetSubCategories(int CatagoriaID)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(_SqlConection))
+            {
+                conn.Open();
+                string query = @"select CONCAT(CodigoSubCategoria,' ' ,NombreSubCategoria) as InfoSubCategoria,
+                                SubCategoriaID
+                                from ConfSubCategorias 
+                                where CategoriasID=@categoriaid";
+                using(SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.Add(new SqlParameter("@categoriaid",SqlDbType.Int)).Value = CatagoriaID;
+                    using (SqlDataAdapter adp = new SqlDataAdapter(cmd))
+                    {
+                        adp.Fill(dt);
+                    }
+                }
+            }
+            return dt;
+        }
+
+
+        public DataTable GetMarcas()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(_SqlConection))
+            {
+                conn.Open();
+                string query = @"SELECT
+	                            CONCAT(CodigoMarca , ' ',NombreMarca) AS InfoMarca, MarcasID 
+	                            FROM  ConfMarcas";
+                using (SqlCommand  cmd = new SqlCommand(query, conn))
+                {
+                    using (SqlDataAdapter adp = new SqlDataAdapter(cmd))
+                    {
+                        adp.Fill(dt);
+                    }
+                }
+            }
+            return dt;
         }
     }
 
--- MMC_Software/ViewModel/ArticulosViewModel.cs
+++ MMC_Software/ViewModel/ArticulosViewModel.cs
@@ -6,15 +6,19 @@ using System.Data;
 using System.Data.SqlClient;
 using System.IO.Packaging;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
 using System.Windows.Navigation;
 
 namespace MMC_Software.ViewModel
 {
     public class ArticulosViewModel : INotifyPropertyChanged
     {
+        public static readonly RepositoryCreacionArticulos RepoCreacionArticulos = new RepositoryCreacionArticulos(
+            ConfiguracionConexion.ObtenerCadenaConexion(ConexionEmpresaActual.BaseDeDatosSeleccionada));
         public int ArticulosID { get; }
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string propertyName)
@@ -27,9 +31,43 @@ namespace MMC_Software.ViewModel
         }
         public ArticulosViewModel()
         {
-
-            //LoadedCategories();
+            LoadedCategories();
+            LoadedMarcas();
         }
+
+
+        /// <summary>
+        ///   METODOS
+        /// </summary>
+
+        public void LoadedMarcas()
+        {
+            if (ArticulosID == 0)
+            {
+                Marca = RepoCreacionArticulos.GetMarcas();
+            }
+        }
+
+        public void LoadedSubCategories()
+        {
+            if (CategoriaSeleccionadaID > 0)
+            {
+                SubCategories = RepoCreacionArticulos.GetSubCategories(CategoriaSeleccionadaID);
+            }
+        }
+
+        public void LoadedCategories()
+        {
+            if (Categories != null && Categories.Rows.Count > 0) return;
+            else
+            {
+                Categories = RepoCreacionArticulos.GetCategorias();
+            }
+        }
+
+        /// <summary>
+        ///       PROPIEDADES DE VIEW MODEL 
+        /// </summary>
 
         private bool _CodigoArticulo = false;
         public bool CodigoArticulo
@@ -51,6 +89,66 @@ namespace MMC_Software.ViewModel
                 OnPropertyChanged(nameof(Categories));
             }
         }
+
+        private DataTable _SubCategories;
+        public DataTable SubCategories
+        {
+            get => _SubCategories;
+            set
+            {
+                if(_SubCategories != value)
+                {
+                    _SubCategories = value;
+                    OnPropertyChanged(nameof(SubCategories));
+                }
+            }
+        }
+
+        private DataTable _Marca;
+        public DataTable Marca
+        {
+            get => _Marca;
+            set
+            {
+                if(_Marca != value)
+                {
+                    _Marca= value; 
+                    OnPropertyChanged(nameof(Marca));
+                }
+            }
+        }
+
+
+        private decimal _IvaCategoria;
+        public decimal IvaCategoria
+        {
+            get => _IvaCategoria;
+            set
+            {
+                if(_IvaCategoria != value)
+                {
+                    _IvaCategoria = value;
+                    OnPropertyChanged(nameof(IvaCategoria));
+                }
+            }
+        }
+
+        private DataRowView _categoriesselection;
+        public DataRowView Categoriesselection
+        {
+            get => _categoriesselection;
+            set
+            {
+                _categoriesselection = value;
+                OnPropertyChanged(nameof(Categoriesselection));
+
+                if(_categoriesselection != null)
+                {
+                    CategoriaSeleccionadaID = Convert.ToInt32(_categoriesselection["CategoriasID"]);
+                    IvaCategoria = Convert.ToDecimal(_categoriesselection["TarifaImpuesto"]);
+                }
+            }
+        }
         private int _categoriaSeleccionadaID;
         public int CategoriaSeleccionadaID
         {
@@ -59,14 +157,31 @@ namespace MMC_Software.ViewModel
             {
                 _categoriaSeleccionadaID = value;
                 OnPropertyChanged(nameof(CategoriaSeleccionadaID));
+
+                //===>Cargo las subcategorias<===//
+                LoadedSubCategories();
             }
         }
-        public void LoadedCategories()
+
+        private int _SubCategoriesSelecctionID;
+        public int SubCategoriesSelecctionID
         {
-            DataTable dt = new DataTable();
-            if (Categories != null && Categories.Rows.Count > 0)
+            get => _SubCategoriesSelecctionID;
+            set
             {
-                return;
+                _SubCategoriesSelecctionID= value;
+                OnPropertyChanged(nameof(SubCategoriesSelecctionID));
+            }
+        }
+
+        private int _marcaSelecctionChanged;
+        public int MarcaSelecctionChanged
+        {
+            get => _marcaSelecctionChanged;
+            set
+            {
+                _marcaSelecctionChanged= value;
+                OnPropertyChanged(nameof(MarcaSelecctionChanged));
             }
         }
     }
